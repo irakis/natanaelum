1ab123b0f5edc887bc27a0c01dd604d2e1a8c959
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2023-08-31 23:17:26 +0200

    about site ready

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,9 +20,9 @@ const About = () => {
                         <div className='col-12 mb-3 mt-3 d-flex justify-content-end'>
                             <h2>Stowarzyszenie</h2>
                         </div>
-                        <div className='col-12 mt-4 d-flex justify-content-end '>
-                            <p className='text-right'>powstało z inicjatywy pracowników Instytutu Psychologii Katolickiego Uniwersytetu Lubelskiego Jana Pawło II. 
-                                Do istnienie powołane sostało na zebraniu załozycielskim w dniu 20.12.2010 roku w Lublinie.
+                        <div className={clsx(styles.description,'col-12 mt-4 d-flex')}>
+                            <p>powstało z inicjatywy pracowników Instytutu Psychologii Katolickiego Uniwersytetu Lubelskiego Jana Pawło II. 
+                                Do istnienia powołane sostało na zebraniu załozycielskim w dniu 20.12.2010 roku w Lublinie.
                                 Stowarzyszenie zarejestrowane zostało przez sąd Rejonowy Lublin-Wschód w Lublinie w dniu 12 maja 2011 roku.
                             </p>
                         </div>
@@ -161,9 +161,9 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className='row d-flex flex-nowrap'>
+            <div className='row d-flex flex-nowrap justify-content-center'>
                 <div className='col-9'>
-                    <p className={clsx(styles.middle)}>Współprace:</p>
+                    <p className={clsx(styles.middle, 'p-4')}>Współprace:</p>
                 </div>
             </div>
             <div className='row justify-content-center'>
@@ -171,11 +171,11 @@ const About = () => {
                     <LogoStamp/>
                 </div>
             </div>
-            <div className={clsx('row', styles.about)}>
-                <div className={clsx(styles.butterflyParent,'col-6 p-0 position-relative')}>
-                    <img className={clsx(styles.butterflyBlue, 'position-absolute')} src={`${process.env.PUBLIC_URL}/images/about/Motyl niebieski 1.png`} alt='motyl_niebieski_1'/>
+            <div className={clsx('row mt-5 pt-5', styles.about)}>
+                <div className={clsx(styles.butterflyParent,'col-7 p-0 position-relative')}>
+                    <img className={clsx(styles.butterflyBlue3, 'position-absolute')} src={`${process.env.PUBLIC_URL}/images/about/Motyl niebieski 1.png`} alt='motyl_niebieski_1'/>
                 </div>
-                <div className={clsx('col-6')}>
+                <div className={clsx('col-5')}>
                     <div className='row d-block h-100'>
                         <div className='col-12 mt-4 mb-4 d-flex justify-content-end'>
                             IPIP
@@ -183,13 +183,15 @@ const About = () => {
                         <div className='col-12 mb-3 mt-3 d-flex justify-content-end'>
                             <h2>Instytut</h2>
                         </div>
-                        <div className='col-12 mt-4 d-flex '>
+                        <div className={clsx(styles.description,'col-12 mt-4 d-flex ')}>
                             <p>Psychoprofilaktyki i Psychoterapii IPIP, powołany do życia w ramach działań statutowych Stowarzyszenia NATANAELUM w dniu 1 czerwca 2011 roku, ma za zadanie dostarczanie merytorycznego wsparcia osobom i instytucjom, które działają 
-                                na rzecz grup i jednostek zagrożonych wykluczeniem społecznym. W szczególności, Instytut koncentruje się na wspieraniu osób uzależnionych od substancji psychoaktywnych i czynności, poprzez prowadzenie badań naukowych 
+                                na rzecz grup i jednostek zagrożonych wykluczeniem społecznym. 
+                                <p></p>
+                                W szczególności, Instytut koncentruje się na wspieraniu osób uzależnionych od substancji psychoaktywnych i czynności, poprzez prowadzenie badań naukowych 
                                 o charakterze epidemiologicznym, społecznym i klinicznym, opracowywanie wyników badań oraz organizowanie szkoleń, warsztatów i innych form edukacji specjalistycznej z zakresu psychologii, psychoprofilaktyki i psychoterapii.
                             </p>
                         </div>
-                        <div className='col-12 mt-5 mb-5 pb-5 d-flex justify-content-end'>
+                        <div className='col-12 mt-3 mb-5 pb-5 d-flex justify-content-end'>
                             <div>
                                 <Button text='Strona instytutu' color='green'/>
                             </div>
@@ -197,11 +199,6 @@ const About = () => {
                     </div>
                 </div>
             </div>
-
-
-
-
-
         </div>
     )
 };
